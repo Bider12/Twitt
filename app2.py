9929daa56b7812e3e440312ea06520d2c1e3a2be
Bider12 <artu.biedermann@gmail.com>
2023-02-27 16:36:57 +0100

    Updated

--- app2.py
+++ app2.py
@@ -9,19 +9,13 @@ from nltk.stem.porter import PorterStemmer
 import re
 from nltk.corpus import stopwords
 
-
-
 #Create Flask app
-
-
 app = Flask(__name__)
 
 #load the pickle model
 model = pickle.load(open("model.pkl","rb"))
 vectorization = pickle.load(open("vector.pkl",'rb'))
-#data = pd.Series(["Trump is king"])
-#vectorization = TfidfVectorizer()
-
+#Function for words optimalization
 def wordopt(text):
     ps = PorterStemmer()
     pattern = re.compile('[^a-zA-Z]')
@@ -31,17 +25,6 @@ def wordopt(text):
     text = [ps.stem(word) for word in text if not word in stopwords.words('english')]
     text = ' '.join(text)
     return text
-#standaryzacja tekstu
-#zamiana w dataframe
-
-
-
-
-
-#xv_test = vectorization.transform(data)
-
-#print(model.predict(data))
-#
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -50,15 +33,11 @@ def home():
 def predict():
     if request.method == 'POST':
         message = request.form['message']
-        #message = "Trump is king"
         message = {'text':[message]}
         df = pd.DataFrame(message)
         df['text'] = df['text'].apply(wordopt)
         message = df['text']
         vectorized = vectorization.transform(message)
-        #prediction = model(vectorized)
-
-
         return render_template('result.html', predict=model.predict(vectorized))
     return render_template("home.html")
 if __name__ == "__main__":
--- tweet.py
+++ tweet.py
@@ -2,68 +2,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from wordcloud import WordCloud
 from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
 from sklearn.model_selection import train_test_split
-from keras.utils import pad_sequences
 from sklearn import model_selection, preprocessing, linear_model, metrics
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn import ensemble
-from lightgbm import LGBMClassifier
-from sklearn.metrics import roc_auc_score, roc_curve
-from xgboost import XGBClassifier
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-from termcolor import colored
-from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 import nltk
-
-
-data = pd.read_csv("data.csv")
-
-print(data.head())
-print(data.isnull().sum())
-print(data.shape)
-
-# 21/4009 = 0.5% missing data, we can delete
-data.dropna()
-data['website'] = data.URLs.apply(lambda x: x.split('/')[2])
-print(data.pivot_table(index = 'website', columns = 'Label', values = 'URLs', aggfunc='count').fillna(0).astype(int))
-
-data['text'] = data['Headline'] + " " + data['Body']
-data = data.drop(columns = ['Headline', 'Body','URLs'])
-
 nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.stem import WordNetLemmatizer
-from collections import Counter
-
-# NLP libraries to clean the text data
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import re
+import typing
 
+#Load the dataset
+data = pd.read_csv("data.csv")
+#Show sample data
+print(data.head())
+#Missing values and shape
+print(data.isnull().sum())
+print(data.shape)
+#21/4009 = 0.5% missing data, we can delete
+data.dropna()
+#Taking main name form websites
+data['website'] = data.URLs.apply(lambda x: x.split('/')[2])
+print(data.pivot_table(index = 'website', columns = 'Label', values = 'URLs', aggfunc='count').fillna(0).astype(int))
+#Join Headline and Body column for one with text
+data['text'] = data['Headline'] + " " + data['Body']
+data = data.drop(columns = ['Headline', 'Body','URLs'])
+#Set stopwords
 stop_words = set(stopwords.words('english'))
-
+#Deleting stopwords, words lower, remove punctation
 ps = PorterStemmer()
 data['text'] = data['text'].str.replace('[^\w\s]', ' ')
 data['text'] = data['text'].apply(lambda x: ' '.join([item for item in str(x).split() if item not in stop_words]))
 data['text'] = data['text'].apply(lambda x: x.lower())
-
-
-
-print(data['text'][1])
-
+#X,Y
 X = data['text']
 Y = data['Label']
 
@@ -74,9 +51,8 @@ x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
 vectorization = TfidfVectorizer()
 xv_train = vectorization.fit_transform(x_train)
 xv_test = vectorization.transform(x_test)
-print(xv_test)
 
-#1. Logistic Regression - used because this model is best suited for binary classification
+#1. Logistic Regression
 LR_model = LogisticRegression()
 
 #Fitting training set to the model
@@ -89,7 +65,7 @@ lr_y_pred = LR_model.predict(xv_test)
 score = accuracy_score(y_test,lr_y_pred)
 print('Accuracy of LR model is ', score)
 
-#2. Support Vector Machine(SVM) - SVM works relatively well when there is a clear margin of separation between classes.
+#2. Support Vector Machine(SVM).
 svm_model = SVC(kernel='linear')
 
 #Fitting training set to the model
@@ -114,7 +90,7 @@ rfc_y_pred = RFC_model.predict(xv_test)
 #Calculate the accuracy score of this model
 score = accuracy_score(y_test,rfc_y_pred)
 print('Accuracy of RFC model is ', score)
-
+#Take the best model and save to pickle file
 model = svm_model.fit(xv_train,y_train)
 print(model)
 print(xv_test)
